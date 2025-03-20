bee3e27b3245a2b1505de60e68f1713b83835c9b
VladUs <vladustyugo@gmail.com>
2025-03-21 00:54:31 +0700

    feat: init server

---
+++ server/src/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+engine = create_async_engine("sqlite+aiosqlite:///tasks.db")
+new_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Model(DeclarativeBase):
+    pass
+
+
+async def create_tables():
+    # https://docs.sqlalchemy.org/en/20/orm/extensions/asyncio.html#synopsis-core
+    async with engine.begin() as conn:
+        await conn.run_sync(Model.metadata.create_all)
+
+
+async def delete_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Model.metadata.drop_all)
---
+++ server/src/models/tasks.py
@@ -0,0 +1,10 @@
+from typing import Optional
+from sqlalchemy.orm import Mapped, mapped_column
+from src.database import Model
+
+class TaskOrm(Model):
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    description: Mapped[Optional[str]]
